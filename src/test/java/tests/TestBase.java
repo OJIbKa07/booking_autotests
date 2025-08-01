21a6f1d4a6fe36adc8ff6e7bbe39b246ffa6274b
Olga Palushina <lokipypzemli@mail.ru>
2025-08-01 18:23:02 +0300

    add template project

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,26 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBase {
+
+    @BeforeAll
+    static void basicBrowserSettings() {
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+        Configuration.baseUrl = "https://www.booking.com/";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browser =  System.getProperty ("browser","chrome");
+        Configuration.browserVersion =  System.getProperty("browser_version", "127.0");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url","selenoid.autotests.cloud/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+}
