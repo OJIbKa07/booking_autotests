b922bc6cc241e0bf68b9584f3fd0ab4291aaf06b
Olga Palushina <lokipypzemli@mail.ru>
2025-08-08 17:20:23 +0300

    change successfulLanguageChange

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -51,7 +51,7 @@ public class MainPage {
                 ),
                 Arguments.of(
                         Language.UK,
-                        List.of("Stays", "Flights", "Flights + Hotel", "Car rental", "Activities", "Airport taxis")
+                        List.of("Stays", "Flights", "Flight + Hotel", "Car rental", "Activities", "Airport taxis")
                 )
         );
     }
