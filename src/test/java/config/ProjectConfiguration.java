c3896d9b6bc2610911fb10c4673c9eac508d9b55
Olga Palushina <lokipypzemli@mail.ru>
2025-10-16 18:28:08 +0300

    DELETE browserSize

--- src/test/java/config/ProjectConfiguration.java
+++ src/test/java/config/ProjectConfiguration.java
@@ -15,7 +15,6 @@ public class ProjectConfiguration {
         Configuration.baseUrl = System.getProperty("baseUrl", config.baseUrl());
         Configuration.browser = System.getProperty("browser", config.browser());
         Configuration.browserVersion = System.getProperty("browserVersion", config.browserVersion());
-        Configuration.browserSize = System.getProperty("browserVersion", config.browserSize());
 
         String remoteUrl = System.getProperty("remoteWebDriverUrl", config.remoteWebDriverUrl());
         
